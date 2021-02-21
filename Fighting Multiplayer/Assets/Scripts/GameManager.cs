75622e1dad46708c237b166fb905d2931a9f986b
reandlyarahdian <52603915+reandlyarahdian@users.noreply.github.com>
2021-02-22 05:03:18 +0700

    Update 1

---
+++ Fighting Multiplayer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class GameManager : Singleton<GameManager>
+{
+    public GameObject inScenePlayer;
+    private PlayerController Controller;
+
+    void Start()
+    {
+        Controller = inScenePlayer.GetComponent<PlayerController>();
+        Activate();
+    }
+
+    void Activate()
+    {
+        Controller.SetupPlayer();
+    }
+}
